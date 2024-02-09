ecce9702aa8e4f9befe9569d6efdb350f972ec77
Matteo Manzoni <matteo.manzoni2@studenti.unimi.it>
2024-02-09 16:42:28 +0100

    fix t coordinates

--- Assets/Scripts/UI/T/TPointText.cs
+++ Assets/Scripts/UI/T/TPointText.cs
@@ -19,6 +19,6 @@ public class TPointText : MonoBehaviour
     private void OnSelectedTChanged(object sender, TSelector.OnSelectedTChangedArgs e)
     {
         var p = e.P / e.P.y;
-        _text.text = $"t = {e.T:F2} \u2192 ({p.z:F2}, {p.x:F2})";
+        _text.text = $"t = {e.T:F2} \u2192 ({p.x:F2}, {p.z:F2})";
     }
 }
